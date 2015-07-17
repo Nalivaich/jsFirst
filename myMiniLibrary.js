fbda90fe448876ed93e3ec2841c7d2f90fa561fa
vitali.nalivaiko <nalivaich93@gmail.ru>
2015-07-17 18:49:41 +0300

    friday :)

--- myMiniLibrary.js
+++ myMiniLibrary.js
@@ -6,3 +6,22 @@
 if (typeof MyMiniLibrary === "undefined") {
  var MyMiniLibrary = {};
 }
+
+
+var p = function() {
+ return 2;
+};
+
+//Супер круто работающая функция
+function extend(Child, Parent) {
+  var F = function() { };
+  F.prototype = Parent.prototype;
+  Child.prototype = new F();
+  Child.prototype.constructor = Child;
+  Child.superclass = Parent.prototype;
+}
+
+
+
+//alert(p.prototype.constructor.prototype.constructor.prototype.constructor());// :)
+
