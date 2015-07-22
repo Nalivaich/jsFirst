8cda6e23dcc44c582b5a97bb5c94deb7d0786831
vitali.nalivaiko <nalivaich93@gmail.ru>
2015-07-22 16:57:04 +0300

    _

--- logFunction/init.js
+++ logFunction/init.js
@@ -4,15 +4,19 @@
 
 require(["jquery", "logMainJS"], function($, logMain) {
 
-    var errorObject =  (new Error());
-    errorObject.message = 'some message';
+    var errorObject =  new Error('Invalid Value');
+    var errorObject2 = new TypeError('TypeError');
+    var errorObject3 = new DOMError('DOMError');
+    var errorObject4 = new EvalError('EvalError');
+    var errorObject5 = new URIError('URIError');
+
     logMain.logFunction(errorObject, 'alerT');
-    logMain.logFunction(errorObject, 'conSole');
+    logMain.logFunction(errorObject2, 'conSole');
     logMain.logFunction(errorObject, 'oFf');
-    logMain.logFunction(errorObject, 'MyGreatApi');
-    logMain.logFunction(errorObject, 'MyGreatApi');
-    logMain.logFunction(errorObject, 'MyGreatApi');
+    logMain.logFunction(errorObject3, 'MyGreatApi');
+    logMain.logFunction(errorObject4, 'MyGreatApi');
+    logMain.logFunction(errorObject5, 'MyGreatApi');
     // можно юзать
-    logMain.addOutputMethod();
-    logMain.setNewRepository();
+    //logMain.addOutputMethod();
+    //logMain.setNewRepository();
 });
